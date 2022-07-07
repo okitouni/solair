73defd0702623e0a3ce0928c9172f8263da16663
kitouni <okitouni@users.noreply.github.com>
2022-07-07 14:49:36 -0400

    implemented binary search. HTC calc.

--- lmtd_simulator.py
+++ lmtd_simulator.py
@@ -1,116 +1,301 @@
+# Great job guys this looks good :)
 import numpy as np
-from typing import Iterable
+from typing import Iterable, Tuple
+from CoolProp.CoolProp import PropsSI
 
 # Designing for 100 MW
 
 
-def get_enthalpy(p, t):
-    # this is a placeholder until we parametrize REFPROP
-    return 800 * np.cos(2 * np.pi * p / t)
+def get_enthalpy(
+    p: Iterable[float], t: Iterable[float], fluid: str = "CO2"
+) -> Iterable[float]:
+    """Compute the enthalpy of a fluid at a given pressure and temperature.
+
+    Args:
+        p (Iterable[float]): Iterable containing the pressures of the fluid in Pa.
+        t (Iterable[float]): Iterable containing the temperatures of the fluid in Kelvin.
+        fluid (str, optional): Fluid to use. Defaults to "CO2".
+
+    Returns:
+        Iterable[float]: Iterable containing the enthalpy of the fluid at the given temperature and pressures in J/kg.
+    """
+    enthalpies = PropsSI("H", "P", p, "T", t, fluid)
+    return enthalpies
+
+
+def compute_air_flow_rate():
+    pass
 
 
 class constants:
-    T_CO2_in = 89
-    T_CO2_out = 35
-    delta_t_CO2 = T_CO2_in - T_CO2_out  # 89 is inlet CO2, 35 is outlet CO2
-
-    T_air_in = 25
+    tolerance = 0.05  # tolerance in heat error to use in temperature search
+    t_co2_inlet = 89 + 273.15
+    t_co2_outlet = 35 + 273.15
+    t_air_inlet = 25 + 273.15
+    p_co2_inlet = 8e6
+    p_co2_outlet = 7.48e6
 
     # calculate air mass flow rate
     # WRITE m_CO2 calculation
     m_CO2 = 950  # [kg/s] for 100MW
-    Q_CO2_tot = m_CO2 * delta_t_CO2
-    cp_air = 1
+    cp_air = 1.005  # [kJ/kg-K]
 
-    enthalpy_table = {
-        (p, t): get_enthalpy(p, t)
-        for p in np.arange(1, 100, 10)
-        for t in np.arange(33, 89)
-    }
+    # calculate total Q exchanged
+    # NEED ENTHALPY OF CO2
+    h_co2_inlet = get_enthalpy(p_co2_inlet, t_co2_inlet)
+    h_co2_outlet = get_enthalpy(p_co2_outlet, t_co2_outlet)
+    total_heat_transferred = abs(m_CO2 * (h_co2_outlet - h_co2_inlet))
 
-    OHTC = 1
+    # find air mass flow rate
+    t_air_outlet = 80 + 273.15  # guess outlet air T, cannot be higher than T_CO2_in
+    delta_t_air = t_air_outlet - t_air_inlet
+    m_air = total_heat_transferred / (
+        cp_air * delta_t_air
+    )  # Assuming something between 6.75 and 30 kg/s which correlates to about 2-5 m/s air velocity
+
+    OHTC = 700  # [W/m2-K]
 
 
-def compute_htc(*params) -> float:
+class Tube:
+    def __init__(self):
+        self.d_ot = 0.025  # [m]          outside tube diameter
+        self.L_t = 9  # [m]  ##TOCHECK     length of tube
+        self.t_f = 5e-4  #  [m]           fin thickness
+        self.p_f = 2.8e-3  # [m]        fin pitch (distance between fins)
+        self.n_f = np.floor(
+            self.L / (self.t_f + self.p_f)
+        )  #         number of fins (per tube at the moment)
+        self.d_f = 57.15e-3  #  [m]     fin outside diameter
+        self.d_r = 0.028 # [m] fin inside diameter
+    def calculate_cross_section_air(self):
+        cross_section_tube_s = np.pi * self.d_ot * (
+            self.L - self.t_f * self.n_f
+        ) + np.pi(
+            (self.d_f ** 2 - self.d_ot ** 2) / 2
+        )  # [m^2]
+        return cross_section_tube_s
+
+    def calculate_surface_area_air(self):
+        surface_area_tube = np.pi * self.d_ot * (
+            self.L - self.t_f * self.n_f
+        ) + np.pi * ((d_f ** 2 - d_ot ** 2) / 2)
+        return surface_area_tube
+
+
+def calculate_re_co2(t, p, m):
+    rho = PropsSI('D', 'T', t, 'P', p,  'CO2')  # density
+    mu = PropsSI('V', 'T', t, 'P', p, 'CO2')    #
+    di = 0.02               # internal diameter of single tube
+    r = di / 2
+    u = m / (rho * (np.pi * r ** 2))
+    L = di
+    Re = (rho * u * L) / mu
+    return Re
+
+def calculate_pr_co2(t, p):
+    Pr = PropsSI('PRANDTL', 'T', t, 'P', p, 'CO2') 
+    return Pr
+
+# WORKING ON THIS
+def calculate_re_air(t, p, m, cross_section_air):
+    rho = PropsSI('D', 'T', t, 'P', p,  'AIR')  # density
+    mu = PropsSI('V', 'T', t, 'P', p, 'AIR')    #
+    return Re
+
+
+def calculate_pr_air(t, p):
+    Pr = PropsSI('PRANDTL', 'T', t, 'P', p, 'AIR') 
+    return Pr
+
+
+def calculate_htc_a(t, p, tube: Tube):
+    """Compute OHTC
+    Args:
+        pr_a (float): Prandtl number.
+        re_a (float): Reynolds number.
+        tube (class element): class describing the geometry of the tube
+    Returns:
+        OHTC: overall heat transfer coefficient for air .
+    """
+    cross_section_air = tube.calculate_cross_section_air()
+    pr_a = calculate_pr_air(t,p)
+    re_a = calculate_re_air(t,p,m,cross_section_air)
+    k_a = 0.025 # W/(mK) thermal conductivity of air #TODO this varies over temperature and pressure
+    htc_a =k_a/tube.d_ot(0.134 * pr_a**(1/3)*re_a**0.681*(2*(tube.p_f-tube.t_f)**0.2/(tube.d_f-tube.d_r))*((tube.p_f-tube.t_f)/t_f)**0.1134)
+    return htc_a
+
+def calculate_htc_s(t,p,m):
+    re_s = calculate_re_co2(p,t,m)
+    pr_s = calculate_pr_co2(p,t)
+    rho_pc = 
+    rho_s = 
+    t_pc = -122.6+6.124*p-0.1657*p**2+0.01773*p**2.5-0.0005608*p**3
+    if t>t_pc:
+        a = 0.14
+        b = 0.69
+        c = 0.66
+        n = 0
+    else:
+        a = 0.013
+        b = 1.0
+        c = -0.05
+        n = 1.6
+    htc_s = a*(re_s**b)*(pr_s**c)*(rho_pc/rho_s)**n 
+    return htc_s
+
+def compute_ohtc(t,p,re_s,pr_s,tube):
+    surface_area_co2 = tube.calculate_surface_area_co2()
+    surface_area_air = tube.calculate_surface_area_air()  # area of the tube
+    htc_a = calculate_htc_a(tube)
+    htc_s = calculate_htc_s(t,p)
+    r_1 = 1 / (surface_area_air * htc_a)  # resistance of cold
+    r_2 = 0  # negliged resistance of wall
+    r_3 = 1 / (surface_area_co2 * htc_s)  # resistance of hot
+    ohtc = 1 / (r_1 + r_2 + r_3)
     pass
 
 
-def lmtd(th: Iterable[float], tc: Iterable[float]) -> float:
-    return ((th[0] - tc[1]) - (th[1] - tc[0])) / np.log(
-        (th[0] - tc[1]) / (th[1] - tc[0])
+def lmtd(t_air_in: float, t_air_out: float, t_co2_in: float, t_co2_out: float) -> float:
+    """ " Compute the LMTD for a given set of temperatures.
+
+    Args:
+        th (Iterable[float]): Iterable containing the initial and final temperatures of the hot fluid.
+        tc (Iterable[float]): Iterable containing the initial and final temperatures of the cold fluid.
+
+    Returns:
+        float: The log mean temperature difference.
+    """
+    return ((t_co2_in - t_air_out) - (t_co2_out - t_air_in)) / np.log(
+        (t_co2_in - t_air_out) / (t_co2_out - t_air_in)
     )
 
 
-def energy_transferred(htc: float, th: Iterable[float], tc: Iterable[float]):
+def energy_transferred(htc: float, th: Iterable[float], tc: Iterable[float]) -> float:
+    """Compute the energy transferred between the hot and cold fluids.
+
+    Args:
+        htc (float): Heat transfer coefficient for the current iteration.
+        th (Iterable[float]): Iterable containing the initial and final temperatures of the hot fluid.
+        tc (Iterable[float]): Iterable containing the initial and final temperatures of the cold fluid.
+
+    Returns:
+        float: The energy transferred between the hot and cold fluids.
+    """
     return htc * lmtd(th, tc)
 
 
-def energy_co2(p1, p0, t1, t0):
-    h0 = get_enthalpy(p0, t0)
-    h1 = get_enthalpy(p1, t1)
-    return constants.m_CO2 * (h1 - h0)
+def energy_co2(p_in: float, p_out: float, t_in: float, t_out: float) -> float:
+    """Compute the energy transferred out of the sCO2.
+
+    Args:
+        p_in (float): Initial pressure of the sCO2.
+        p_out (float): Final pressure of the sCO2.
+        t_in (float): Initial temperature of the sCO2.
+        t_out (float): Final temperature of the sCO2.
+
+    Returns:
+        float: The energy transferred out of the sCO2.
+    """
+    h_in = get_enthalpy(p_in, t_in)
+    h_out = get_enthalpy(p_out, t_out)
+    return constants.m_CO2 * abs(h_out - h_in)
 
 
-def energy_air(mdot, t1, t0):
+def energy_air(mdot: float, t0: float, t1: float) -> float:
+    """Compute the energy transferred into the air.
+
+    Args:
+        mdot (float): Mass flow rate of the air.
+        t0 (float): Temperature of the inlet air.
+        t1 (float): Temperature of the outlet air.
+
+    Returns:
+        float: The energy transferred into the air.
+    """
     return constants.cp_air * mdot * (t1 - t0)
 
-def p_out(p_in):
+
+def drop_pressure(p_in: float) -> float:
+    """Compute the pressure drop of the sCO2 across an element and returns the outlet pressure.
+
+    Args:
+        p_in (_type_): Initial pressure of the sCO2.
+
+    Returns:
+        float: The output pressure of the sCO2.
+    """
+    # TODO figure out constants
     rho = 1
-    f = 1 # from Eq. 16
+    f = 0.112  # from Eq. 16
     u = 1
     L = 1
     d = 1
-    delta_p = (rho*f*u**2*L)/(2*d)
+    delta_p = (rho * f * u ** 2 * L) / (2 * d)
     p_out = p_in - delta_p
-    return p_out
-
-# calculate total Q exchanged
-# NEED ENTHALPY OF CO2
-h_CO2_in = constants.enthalpy_table[(1, 33)]
-h_CO2_out = constants.enthalpy_table[(1, 34)]
-Q = constants.m_CO2 * (h_CO2_out - h_CO2_in)
-
-# find air mass flow rate
-T_air_out = (constants.T_CO2_in + constants.T_CO2_out) / 2  # guess outlet air T
-delta_t_air = T_air_out - constants.T_air_in
-m_air = Q / (constants.cp_air * delta_t_air)
+    return p_in  # TODO figure out how to compute pressure drop
 
 
-##################################
 
-P_11 = 8 # from previous step
-P_21 = p_out(P_11) 
-Th_11 = 89 # known from cycle
+def temperature_search(
+    p_co2_in: float, t_co2_in: float, t_air_in: float, max_depth: int = 20
+) -> Tuple[float]:
+    """Search for the temperature of the sCO2 in a given segment.
 
-# guess Th_i_j+1
-Th_21 = 89 # make better guess
+    Args:
+        p_co2_in (float): Initial pressure of the sCO2. 
+        t_co2_in (float): Initial temperature of the sCO2.
+        t_air_in (float): Initial temperature of the air.
+        max_depth (int, optional): Max iteration depth in the search. Defaults to 20.
 
-################ LOOP
+    Returns:
+        Tuple[float]: The final temperature of the sCO2 and air across the segment.
+    """
+    # Currently, this is a binary search and only works when data is sorted 
+    # (i.e. q_htc - q_co2 is monotonic in sCO2 output temp).
+    def binary_search_temps(left, right, max_depth):
+        if max_depth == 0:
+            raise RecursionError("Max depth reached")
+        t_co2_out = (left + right) / 2
+        p_co2_out = drop_pressure(p_co2_in)
+        q_co2 = energy_co2(p_co2_in, p_co2_out, t_co2_in, t_co2_out)
+        # TODO get m_air for segment
+        m_air_segment = constants.m_air
+        # compute air out temp from heat transfer into air
+        t_air_out = q_co2 / (m_air_segment * constants.cp_air) + t_air_in
 
-# calculate Q1
+        if t_air_out > t_co2_in:
+            raise Exception("Air temperature is too high. Physics is broken. :(")
 
-h_11 = constants.enthalpy_table[(P_11, Th_11)]
-h_21 = constants.enthalpy_table[(P_21, Th_21)]
-Q1_11 = constants.m_CO2*(h_11 - h_21) # find h from tables
+        delta_t_m = lmtd(t_air_in, t_air_out, t_co2_in, t_co2_out)
+        q_htc = constants.OHTC * (delta_t_m)  # TODO OHTC is not constant
 
-Tc_11 = constants.T_air_in # change if inside loop
+        if abs((q_htc - q_co2) / q_co2) > constants.tolerance:
+            if q_htc > q_co2:
+                # too little co2 heat change -> increase delta T -> lower outlet temp
+                # -> set current middle to be the new max
+                right = t_co2_out
+            else:
+                left = t_co2_out
+            return binary_search_temps(left, right, max_depth - 1)
+        else:
+            return t_co2_out, t_air_out
 
-Tc_12 = Q1_11 /(m_air*constants.cp_air) + Tc_11
+    # iterate until temperature converges.
+    t_co2_out, t_air_out = binary_search_temps(t_air_in, t_co2_in, max_depth)
+    return t_co2_out, t_air_out
 
-# calculate delta_T_m
-th = [Th_11,Th_21]
-tc = [Tc_11,Tc_12]
-delta_T_m = lmtd(th,tc)
 
-Q2_11 = constants.OHTC*(delta_T_m)
 
-# compare Q1 Q2
-
-# update Th_1_2
-
-################# 
+print(
+    temperature_search(
+        p_co2_in=constants.p_co2_outlet, # start from the end of the tube
+        t_co2_in=constants.t_co2_inlet,
+        t_air_in=constants.t_air_inlet,
+    )
+)
+#################
 
 
 # express Q1 -Q2 as fct of guessed temperature th_i+1_j
 
-# Q1-Q2 = (OHTC * ( ) ) / () 
+# Q1-Q2 = (OHTC * ( ) ) / ()
