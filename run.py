ba49ae422b94270ee5a27d658f82d187e1def1cd
kitouni <28654657+okitouni@users.noreply.github.com>
2022-07-12 19:46:57 +0000

    [refactor] simulation run

---
+++ run.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+from lmtd_simulator import Simulator
+from constants import constants
+
+
+parser = ArgumentParser()
+parser.add_argument("-n", "--n_segments", type=int, default=100)
+parser.add_argument("-v", "--verbose", type=int, default=2)
+parser.add_argument("-d", "--max_depth", type=int, default=20)
+
+
+
+def main():
+    args = parser.parse_args()
+    simulator = Simulator()
+    print(constants.t_air_inlet)
+    simulator.run(constants.t_co2_outlet, constants.t_air_inlet, max_segments=args.n_segments, verbose=args.verbose)
+    return 1
+
+if __name__ == "__main__":
+    main()
