fb278efbe963669d053759386925fdde2fa8f93d
okitouni <okitouni@users.noreply.github.com>
2022-08-01 13:11:30 +0000

    added nb with cost comparison and output of sample optimization run

--- optimization.py
+++ optimization.py
@@ -1,4 +1,4 @@
-from turbo import Turbo1
+from turbo import Turbo1, TurboM
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -12,27 +12,28 @@ torch.manual_seed(0)
 torch.cuda.manual_seed_all(0)
 np.random.seed(0)
 
+
 class Csp:
     def __init__(self):
         # tube_in_diameter, tube_outer_inner_diff, fin_in_diameter, fin_outer_inner_diff
         self.lb = np.array(
             [
                 20e-3 - 10e-3,  # tube_in_diameter
-                1.1, # tube_out_diameter = fraction of tube_in_diameter
-                1.1,  # fin_in_diameter = fraction of tube_out_diameter
-                1.1,  # fin_out_diameter = fraction of fin_in_diameter
-                1e-3, # fin_pitch
-                0.1, # fin_thickness = fraction of fin_pitch
+                1.1,  # tube_out_diameter = multiple of tube_in_diameter
+                1.1,  # fin_in_diameter = multiple of tube_out_diameter
+                1.1,  # fin_out_diameter = multiple of fin_in_diameter
+                1e-3,  # fin_pitch
+                0.1,  # fin_thickness = fraction of fin_pitch
             ]
         )
         self.ub = np.array(
             [
                 20e-3 + 20e-3,  # tube_in_diameter
-                2, # tube_out_diameter  - tube_in_diameter
-                2, # fin_in_diameter = fraction of tube_out_diameter
-                2, # fin_out_diameter = fraction of fin_in_diameter
-                4e-3, # fin_pitch
-                .8, # fin_thickness = fraction of fin_pitch
+                2,  # tube_out_diameter = multiple of tube_in_diameter
+                2,  # fin_in_diameter = multiple of tube_out_diameter
+                2,  # fin_out_diameter = multiple of fin_in_diameter
+                4e-3,  # fin_pitch
+                0.8,  # fin_thickness = fraction of fin_pitch
             ]
         )
 
@@ -42,7 +43,7 @@ class Csp:
         assert np.all(x <= self.ub) and np.all(x >= self.lb)
         tube_in_diameter = x[0]
         tube_out_diameter = tube_in_diameter * x[1]
-        fin_in_diameter = tube_out_diameter  * x[2]
+        fin_in_diameter = tube_out_diameter * x[2]
         fin_out_diameter = fin_in_diameter * x[3]
         fin_pitch = x[4]
         fin_thickness = x[5] * fin_pitch
@@ -71,17 +72,22 @@ class Csp:
 
 
 if __name__ == "__main__":
-    from datetime import datetime
-    import os
-    from pathlib import Path
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument("-n", "--n_evals", type=int, default=100)
+    parser.add_argument("-o", "--output", type=str, default="output")
+    parser.add_argument("-m", "--turbo_m", action="store_true")
+    args = parser.parse_args()
 
     f = Csp()
-    turbo1 = Turbo1(
+    Turbo = TurboM if args.turbo_m else Turbo1
+    kwargs = dict(
         f=f,  # Handle to objective function
         lb=f.lb,  # Numpy array specifying lower bounds
         ub=f.ub,  # Numpy array specifying upper bounds
         n_init=20,  # Number of initial bounds from an Latin hypercube design
-        max_evals=1000,  # Maximum number of evaluations
+        max_evals=args.n_evals,  # Maximum number of evaluations
         batch_size=10,  # How large batch size TuRBO uses
         verbose=True,  # Print information from each batch
         use_ard=True,  # Set to true if you want to use ARD for the GP kernel
@@ -89,11 +95,11 @@ if __name__ == "__main__":
         n_training_steps=50,  # Number of steps of ADAM to learn the hypers
         min_cuda=1024,  # Run on the CPU for small datasets
         device="cpu",  # "cpu" or "cuda"
-        dtype="float64",  # float64 or float32
+        dtype="float32",  # float64 or float32 # Number of trust regions
     )
+    if args.turbo_m:
+        kwargs["n_trust_regions"] = 10
+    turbo1 = Turbo(**kwargs)
 
     turbo1.optimize()
-    time_stamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    save_path = Path(os.getcwd()) / "results" / time_stamp
-    os.mkdir(save_path, exist_ok=True)
-    np.savez("fit_results.npz", X=turbo1.X, fX=turbo1.fX)
+    np.savez(f"{args.output}.npz", X=turbo1.X, fX=turbo1.fX)
